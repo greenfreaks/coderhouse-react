375b20ebd72b9f7f33c206d480c336bb2d9e0c47
Mario Sandoval Velázquez <mariosandovalv1998@gmail.com>
2022-12-27 16:05:43 -0600

    Desafío de la 3era clase JSX-TRANSPILING: se crea navbar

---
+++ 3-jsx-transpiling/desafio-jsx-transpiling/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div><h3>GREEN FREAKS</h3></div>
+            <div className="links">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Nosotros</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Contacto</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
